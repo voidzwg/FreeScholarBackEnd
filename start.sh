bdf4e3e6d03186bb7686cddf5b199cb7affb77dc
庄伟光 <1454101799@qq.com>
2022-11-19 13:15:34 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py makemigrations && python manage.py migrate && uwsgi --ini /var/www/html/backend/uwsgi.ini
+python manage.py makemigrations && python manage.py migrate && uwsgi --ini /var/www/html/backend/uwsgi.ini && python manage.py runserver 8000
