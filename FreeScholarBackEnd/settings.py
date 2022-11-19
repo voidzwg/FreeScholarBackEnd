4de6516edb6b5250601de36b7aa6b2aa0c2c4fa0
庄伟光 <1454101799@qq.com>
2022-11-19 13:02:00 +0800

    update .gitignore

--- FreeScholarBackEnd/settings.py
+++ FreeScholarBackEnd/settings.py
@@ -130,4 +130,18 @@ CORS_ALLOW_CREDENTIALS = True
 # 添加允许执行跨站点请求的主机，为True，则将不使用白名单，并且将接受所有来源
 CORS_ORIGIN_ALLOW_ALL = True
 # 允许所有的请求头
-CORS_ALLOW_HEADERS = '*'
+CORS_ALLOW_HEADERS = ('*', )
+
+CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:8000',
+)
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+    'VIEW',
+)
