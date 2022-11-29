6d11e8f42f697282bbc3c1c984d4426f2b82be9f
庄伟光 <1454101799@qq.com>
2022-11-29 23:15:00 +0800

    数据库脱敏处理

--- FreeScholarBackEnd/settings.py
+++ FreeScholarBackEnd/settings.py
@@ -9,11 +9,14 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import json
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+json_file = open("../secrets.json")
+SECRETS = json.load(json_file)
 
 
 # Quick-start development settings - unsuitable for production
@@ -77,8 +80,15 @@ WSGI_APPLICATION = 'FreeScholarBackEnd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': SECRETS.get("HOST"),
+        'PORT': SECRETS.get("PORT"),
+        'NAME': SECRETS.get("NAME"),
+        'USER': SECRETS.get("USER"),
+        'PASSWORD': SECRETS.get("PASSWORD"),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
