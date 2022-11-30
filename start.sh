c8747404c044da6c8e7aecdbcc45c212eee91695
庄伟光 <1454101799@qq.com>
2022-11-30 20:08:01 +0800

    修改start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-python /var/www/html/backend/manage.py makemigrations && python /var/www/html/backend/manage.py migrate && uwsgi --ini /var/www/html/backend/uwsgi.ini && python /var/www/html/backend/manage.py runserver 8000
+python /var/www/html/backend/manage.py makemigrations
+python /var/www/html/backend/manage.py migrate
+uwsgi --ini /var/www/html/backend/uwsgi.ini
+python /var/www/html/backend/manage.py runserver 8001
