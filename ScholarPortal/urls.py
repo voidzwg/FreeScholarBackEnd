82401f82d8b0be6d40f0c4833fd279e4b5b672a6
庄伟光 <1454101799@qq.com>
2022-12-01 13:05:42 +0800

    修改urls.py

--- ScholarPortal/urls.py
+++ ScholarPortal/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from .models import Scholarportal
 from .views import GetBaseInfo
 
 urlpatterns = [
-    path('GetBaseInfo/', GetBaseInfo.as_view(model=Scholarportal)),
+    path('GetBaseInfo/', GetBaseInfo.as_view()),
 ]
