f33e38789daeea99f64130530865db47556b7e5f
lxj <810607510@qq.com>
2022-12-13 20:42:00 +0800

    2222

--- publication/views.py
+++ publication/views.py
@@ -345,33 +345,32 @@ class publication:
 
     def MakeComment(request):
         if request.method == 'POST':
-            if request.method == 'POST':
-                fail, payload = Authentication.authentication(request.META)
-                if fail:
-                    return JsonResponse(payload)
-                try:
-                    user_id = payload.get('id')
-                    user = User.objects.get(field_id=user_id)
-                except User.DoesNotExist:
-                    return JsonResponse({'errno': 1, 'msg': "用户不存在"})
-                try:
-                    data_body = request.POST
-                    paper_id = data_body.get('paper_id')
-                    content = data_body.get('content')
-                    paper = Paper.objects.filter(paper_id=paper_id).first()
-                    if paper is None:
-                        return JsonResponse({'error': 0, 'message': "文章不存在"})
-                    else:
-                        comment = Comment()
-                        comment.user = user
-                        comment.paper_id = paper_id
-                        comment.count = 0
-                        comment.content = content
-                        comment.create_time = datetime.datetime
-                        comment.save()
-                    return JsonResponse({'message': "评论成功"})
-                except Exception as e:
-                    traceback.print_exc()
+            fail, payload = Authentication.authentication(request.META)
+            if fail:
+                return JsonResponse(payload)
+            try:
+                user_id = payload.get('id')
+                user = User.objects.get(field_id=user_id)
+            except User.DoesNotExist:
+                return JsonResponse({'errno': 1, 'msg': "用户不存在"})
+            try:
+                data_body = request.POST
+                paper_id = data_body.get('paper_id')
+                content = data_body.get('content')
+                paper = Paper.objects.filter(paper_id=paper_id).first()
+                if paper is None:
+                    return JsonResponse({'error': 0, 'message': "文章不存在"})
+                else:
+                    comment = Comment()
+                    comment.user = user
+                    comment.paper_id = paper_id
+                    comment.count = 0
+                    comment.content = content
+                    comment.create_time = datetime.datetime.now()
+                    comment.save()
+                return JsonResponse({'message': "评论成功"})
+            except Exception as e:
+                traceback.print_exc()
         else:
             return JsonResponse({'error': 0, 'message': "请求方式错误"})
 
@@ -396,7 +395,7 @@ class publication:
                     like11 = Like1()
                     like11.comment = comment
                     like11.user = user
-                    like11.create_time = datetime.datetime
+                    like11.create_time = datetime.datetime.now()
                     like11.save()
                     comment.count += 1
                     comment.save()
